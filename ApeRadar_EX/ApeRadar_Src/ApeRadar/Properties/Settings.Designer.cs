3f7d92160df498b019f2e37dd274e71fcc9b9b18
slowpoke0520 <76835969+slowpoke0520@users.noreply.github.com>
2026-09-11 20:19:28 +0800

    release: 2.1.1-ex.10

--- ApeRadar_EX/ApeRadar_Src/ApeRadar/Properties/Settings.Designer.cs
+++ ApeRadar_EX/ApeRadar_Src/ApeRadar/Properties/Settings.Designer.cs
@@ -34,7 +34,7 @@ namespace ApeRadar.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("20260909")]
+        [global::System.Configuration.DefaultSettingValueAttribute("20260911")]
         public string SoftwareDate {
             get {
                 return ((string)(this["SoftwareDate"]));
