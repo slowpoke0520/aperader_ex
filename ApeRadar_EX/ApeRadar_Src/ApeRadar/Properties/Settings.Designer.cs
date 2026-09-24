f498e2d3f0595849abe3c54080c82a61b80c9ba7
slowpoke0520 <76835969+slowpoke0520@users.noreply.github.com>
2026-09-24 16:38:46 +0800

    Fix dashboard player context menu crash

--- ApeRadar_EX/ApeRadar_Src/ApeRadar/Properties/Settings.Designer.cs
+++ ApeRadar_EX/ApeRadar_Src/ApeRadar/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace ApeRadar.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("2.1.1-ex.12-dev.15")]
+        [global::System.Configuration.DefaultSettingValueAttribute("2.1.1-ex.12-dev.16")]
         public string SoftwareVersion {
             get {
                 return ((string)(this["SoftwareVersion"]));
@@ -34,7 +34,7 @@ namespace ApeRadar.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("20260923")]
+        [global::System.Configuration.DefaultSettingValueAttribute("20260924")]
         public string SoftwareDate {
             get {
                 return ((string)(this["SoftwareDate"]));
